01344c4cfc496414109ed990668f05df6ea336ca
dcfvg <qwsx@dcfvg.com>
2014-02-15 21:53:03 +0100

    Add comment and led feedback

--- apps/life-controller/tests/niveau_electrode/niveau_electrode.ino
+++ apps/life-controller/tests/niveau_electrode/niveau_electrode.ino
@@ -1,6 +1,8 @@
+
 const int levelSensorPin = 2;
 const int motorPinA = 9;
 const int motorPinB = 6;
+const int led = 13;
 
 int sensorState = 0, timer = 0;
 boolean pause = false, sec = true;
@@ -9,28 +11,32 @@ void setup(){
   pinMode(levelSensorPin, INPUT);
   pinMode(motorPinA,OUTPUT);
   pinMode(motorPinB,OUTPUT);
+  pinMode(led, OUTPUT);
 }
+
 void loop(){ 
 
-  // IN / maintaining level
+  digitalWrite(led, LOW);         // run monitor 
+  
+  // ADD / maintaining level
+  
   sensorState = digitalRead(levelSensorPin);
+  
   if (sensorState==LOW)digitalWrite(motorPinA, HIGH);
   else digitalWrite(motorPinA, LOW);
 
-  // OUT / random 
-
+  // GET / random
+  
   if(timer == 0) {
     timer = random(1 , 12) * 10; // random time
-    pause = !pause;       // pause or pumping mode 
+    pause = !pause;              // pause or pumping mode 
   }else{ 
-    timer --;
+    timer --;                    // count time
   }
+  
+  if (timer > 0 && ! pause) digitalWrite(motorPinB, HIGH); // pumping only if timer is not finished and not in pause
+  else digitalWrite(motorPinB, LOW); 
 
-  if (timer > 0 && ! pause) digitalWrite(motorPinB, HIGH);
-  else digitalWrite(motorPinB, LOW);
-
-  delay(1000);
+  digitalWrite(led, HIGH);       // run monitor 
+  delay(1000);                   // skectch resolution = 1s
 }
-
-
-
