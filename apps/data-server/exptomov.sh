6f676f032ef9d6c13ed372623f65380a45837373
dcfvg <qwsx@dcfvg.com>
2014-02-15 15:42:31 +0100

    say images count

--- apps/data-server/exptomov.sh
+++ apps/data-server/exptomov.sh
@@ -7,8 +7,12 @@ path="$(dirname $0)/../../assets/captation/exp"
 cd $path
 
 while true; do
-  echo "Refresh live movie "
-    
+  imagescount=$(find $path -type f -iname "*.JPG" -exec printf '.' \; | wc -c  | tr -d ' ')
+  
+  say "$imagescount images"
+  
+  echo "Refresh live movie ($imagescount)"
+  
 	ffmpeg -loglevel panic -f image2 -pattern_type glob -i '*.JPG' -r 25 -vcodec mpeg4 -b 30000k -vf scale=1920:-1 -y tmp.mp4
 	cp -f tmp.mp4 live.mp4
 	sleep $freq
