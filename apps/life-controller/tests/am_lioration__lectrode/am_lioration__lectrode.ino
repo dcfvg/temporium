d32eeed181727fec0b5a432a557c72b0b6242248
Glogzy <arnaud.gloaguen@supelec.fr>
2014-02-16 22:40:57 +0100

    First version

--- apps/life-controller/tests/am_lioration__lectrode/am_lioration__lectrode.ino
+++ apps/life-controller/tests/am_lioration__lectrode/am_lioration__lectrode.ino
@@ -1,70 +1,92 @@
 const int levelSensorPin = A0;
 const int levelButtonPin = 2;
-const int motorPinA = 9;
-const int motorPinB = 6;
-const int motorPinC= 3;
+const int motorPinBioToAq = 6; // bioreactor to aquarium 
+const int motorPinFoodToBio = 9; // food to the bioreactor
+const int motorPinAqToTrash = 3; // aquarium to trash
 const int led = 13;
 const int R = 10030; // Résistance utilisée pour le montage pont diviseur de tension (en ohms)
 const int V = 5; //Valeur de l'alimentation de l'Arduino, peu précis mais pas important pour le niveau de travail (en volts)
+const int buttonMemory = 0;
+const int levelElectrode = 0;
 
-int buttonMemory = 0;
-int levelElectrode = 0;
-
+boolean addAlgue = false;
+boolean bioReactorFull = false;
+boolean prevButtonsensorState = false;
+boolean electroProtect = false;
+int buttonsensorState = 0;
 
 void setup(){
   pinMode(levelSensorPin, INPUT);
   pinMode(levelButtonPin, INPUT);
-  pinMode(motorPinA,OUTPUT);
-  pinMode(motorPinB,OUTPUT);
-  pinMode(motorPinC,OUTPUT);
+  pinMode(motorPinBioToAq,OUTPUT);
+  pinMode(motorPinFoodToBio,OUTPUT);
+  pinMode(motorPinAqToTrash,OUTPUT);
   pinMode(led, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop(){ 
 
-  digitalWrite(led, LOW);         // run monitor 
-  int buttonsensorState = digitalRead(levelButtonPin);
-  
-  // ADD / maintaining level
-  
-  while (buttonsensorState==HIGH){
-    digitalWrite(motorPinA, HIGH);
-    buttonMemory = 1;
-  }
+  Serial.print("bioReactorFull:" + String(bioReactorFull) + " \t " + "addAlgue:" + String(addAlgue)  + " \t " + "electroProtect:" + String(electroProtect) +"\n");
  
-  digitalWrite(motorPinA, LOW);
+  // run monitor 
+  digitalWrite(led, LOW);        
+  delay(100);
+  digitalWrite(led, HIGH); 
+
+  // check user action 
+  buttonsensorState = digitalRead(levelButtonPin);
+  if (buttonsensorState && !prevButtonsensorState){ // check previous button state to prevent double click
+    addAlgue = !addAlgue;  // toogle state
+    Serial.print("AddAlague" + String(addAlgue));
+  }
   
-  while (buttonMemory==1 && buttonsensorState==LOW){
-    
+  // add alguae to the aquarium
+  if(addAlgue && bioReactorFull){
+    digitalWrite(motorPinBioToAq, HIGH);
+  }else{
+    digitalWrite(motorPinBioToAq, LOW);
+    bioReactorFull = false;
+  }
+  
+  // add food 
+  if(!bioReactorFull) {
     double V_mes = (analogRead(levelSensorPin)/1024.0)*5.0; //tension mesurée aux bornes de l'électrode
     double R_mes = (R*V_mes)/(V-V_mes); //mesure de la résistance de l'électrode
-  
+
     Serial.print("V_mes = ");
     Serial.print(V_mes);
     Serial.print("\n R_mes = ");
     Serial.print(R_mes);
-    
-    while (R_mes < 1  && buttonsensorState==LOW){
-      digitalWrite(motorPinB, HIGH);
-      //Serial.print(la pompe est allumée)
-      levelElectrode = 1;
-      
+
+    if (R_mes < 1){
+      digitalWrite(motorPinFoodToBio, HIGH);
+    }else {
+      digitalWrite(motorPinFoodToBio, LOW);
+      bioReactorFull = true;
+      electroProtect = true;
     }
+  }
+  // lower level to protect solution from electrod
+  if (electroProtect){
+    digitalWrite(motorPinBioToAq, HIGH);
+    digitalWrite(motorPinAqToTrash, HIGH);
     
-    digitalWrite(motorPinB, LOW);
+    delay(5000);
     
-    if (levelElectrode == 1 && buttonsensorState==LOW){
-      digitalWrite(motorPinA, HIGH);
-      digitalWrite(motorPinC, HIGH);
-      levelElectrode = 0;
-      buttonMemory = 0;
-      delay(5000);
-     }
-   }
- }
+    digitalWrite(motorPinBioToAq, LOW);
+    digitalWrite(motorPinAqToTrash, LOW);
+    
+    electroProtect = false;
+    bioReactorFull = true;
+    addAlgue = false;
+  }
+  prevButtonsensorState = buttonsensorState;
+}
 
 
- 
-    
-    
+
+
+
+
+
