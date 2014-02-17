ff3ac9cd0dc48b56f44e7c1fcdae8b287fb6f3bc
AubreyC <Cactus@Cactuss-MacBook-Pro.local>
2014-02-17 19:49:24 +0100

    TEST arduino

--- apps/life-controller/visual_feedback/test_arduino/test_arduino.ino
+++ apps/life-controller/visual_feedback/test_arduino/test_arduino.ino
@@ -8,8 +8,16 @@ i = 0;
 void loop()
 {
 //send 'Hello, world!' over the serial port
-if (i<100) Serial.write(121);
-else Serial.write(120);
+if (i<100) {
+  Serial.write(61);
+  Serial.write(91);
+  Serial.write(31);
+}
+else {
+  Serial.write(60);
+  Serial.write(90);
+  Serial.write(30);
+}
 i = i+1;
 //wait 100 milliseconds so we don't drive ourselves crazy
 delay(100);
