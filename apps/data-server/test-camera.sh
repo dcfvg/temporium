2f10c8b9b22e7795994fb90d4bd3a032010b5d33
dcfvg <qwsx@dcfvg.com>
2014-02-20 23:42:58 +0100

    add basic procedure

--- apps/data-server/test-camera.sh
+++ apps/data-server/test-camera.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 #set -x
 
+# make sure the camera is available.
+killall PTPCamera 
+
+# launch detection
+gphoto2 --auto-detect
+gphoto2 --summary
+
+# capture
+gphoto2 --capture-image
+gphoto2 --capture-image-and-download
