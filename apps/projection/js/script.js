420eb197d69ce3610c08ec27f877ce27b246685b
dcfvg <qwsx@dcfvg.com>
2014-05-10 17:36:21 +0200

    add osc events

--- apps/projection/js/script.js
+++ apps/projection/js/script.js
@@ -22,6 +22,18 @@ $(function() {
 		$life.removeClass("off");
 		$movie.addClass("off");
 	}
+	function blackScreen(){
+		$life.addClass("off");
+		$movie.addClass("off");
+	}
+	function reset (){
+		blackScreen();
+		
+		// seek to beginning 
+		$pop_movie.currentTime(0);	
+		$pop_life.currentTime(0)
+		
+	}
 	function reloadLife(){
 		// add reload argument to avoid cache
 		$life.attr("src","assets/life.mp4?reload="+Math.round((new Date()).getTime() / 1000)).load();
@@ -32,46 +44,66 @@ $(function() {
 	}
 
 	// OSC 
-	
 	socket = io.connect('http://127.0.0.1', { port: 8081, rememberTransport: false});
-  console.log('oi');
-  socket.on('connect', function() {
-       // sends to socket.io server the host/port of oscServer
-       // and oscClient
-       socket.emit('config',
-           {
-               server: {
-                   port: 3333,
-                   host: '127.0.0.1'
-               },
-               client: {
-                   port: 3334,
-                   host: '127.0.0.1'
-               }
-           }
-       );
-   });
+	socket.on('connect', function() {
+		// sends to socket.io server the host/port of oscServer
+		// and oscClient
+		socket.emit('config',
+		{
+			server: {
+				port: 3333,
+				host: '127.0.0.1'
+			},
+			client: {
+				port: 3334,
+				host: '127.0.0.1'
+			}
+		});
+	});
+  socket.on('message', function(obj) {
+      console.log(obj);
 
-   socket.on('message', function(obj) {
-       console.log(obj);
-   });
+			switch (obj[0]) {
+			case "/seance_start":
+				
+				showMovie();
+				$pop_movie.play();
+			
+			break;
+			case "/life_reload":
+				reloadLife();
+			break;
+			case "AHAHA":
+			    day = "Monday";
+			break;
+				
+			default:
+				text ="hein ?";
+			}
+  });
 
 	// cue manager
 		
 	$pop_movie.cue( 2, function() {
-			console.log( "from:"+this.currentTime() );
 	    this.currentTime( 10.5 ).play();
-			console.log( "to:"+this.currentTime() );
 	});
 	$pop_movie.cue( 12, function() {
-			console.log( "from:"+this.currentTime());
+			$pop_life.play();
 			showLife();
 	});
-	
 	$pop_movie.cue( 20, function() {
 			console.log( "from:"+this.currentTime());
 			showMovie();
 	});
+	
+	// player event 
+	
+	$movie.on("ended", function() {
+			console.log("seance_end ! ");
+			blackScreen();
+			socket.emit('1', '/seance_end');
+	});
+	
 	// shortcuts
 	$(document).keypress(function( event ){
     //console.log(event.which);
@@ -92,16 +124,5 @@ $(function() {
 		
 	
   });
-
-	var server = io.connect('http://localhost/osc/servers/8000'),
-	  client = io.connect('http://localhost/osc/clients/8000');
-
-	server.on('message', function(message) {
-	  console.log(message);
-	});
-
-	setInterval(function() {
-	  client.emit('message', ['/osc/test', 200]);
-	}, 500);
 
 });
