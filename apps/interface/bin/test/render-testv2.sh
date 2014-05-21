861a658aba41c733f2a8dc4175305564aae4279b
dcfvg <qwsx@dcfvg.com>
2014-05-21 21:13:59 +0200

    ffmpeg filters

--- apps/interface/bin/test/render-testv2.sh
+++ apps/interface/bin/test/render-testv2.sh
@@ -1,19 +1,20 @@
-function timelaps_render {
-  # lauch timelaps render script ( render the animation with ffmpeg)
-	
-  cd $exp
-	
-	speed=${1-1}
 
-	# TODO : adapt to movie size : 1888 x 1062 
+exp=exp
+cd $exp
 
-  ffmpeg -loglevel panic -f image2 -pattern_type glob -i '*.jpg' -r 25 -vcodec mpeg4 -b 30000k -vf scale=1888:-1 -y tmp.mp4
-  
-  # replace live movie
-  cp -f tmp.mp4 live.mp4
+speed=${1-1}
+zoom=${2-1}
 
-	oscSend	life_reload 1
-}
+# TODO : adapt to movie size : 1888 x 1062 
 
-exp=../assets/exp
-timelaps_render 1
+ffmpeg -f image2 \
+-pattern_type glob -i '*.jpg' \
+-c:v libx264 -preset ultrafast -qp 0 \
+-r 25 -vf "scale=6000:-1, crop=1888:1062:0:0, setpts=(1/1)*PTS" \
+-y tmp.mp4
+
+# -framerate 25  \
+
+# replace live movie
+cp -f tmp.mp4 live.mp4
+
