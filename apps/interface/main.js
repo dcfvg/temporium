5e98394ed5d44120a6995fc68f82c626646149c2
dcfvg <qwsx@dcfvg.com>
2014-05-21 22:18:17 +0200

    ffmpeg integration + clean

--- apps/interface/main.js
+++ apps/interface/main.js
@@ -1,47 +1,64 @@
-var fs = require('fs');
-
-var osc = require('node-osc');
-var util = require('util');
-
-
-var oscClient = new osc.Client('127.0.0.1', 3334);
+var fs = require('fs'),
+    osc = require('node-osc'),
+    util = require('util'),
+    ffmpeg = require('fluent-ffmpeg'),
+    oscClient = new osc.Client('127.0.0.1', 3334);
 
 module.exports = function(app, io, oscServer){
-  console.log("main module initialized");
+  console.log("movie <> temporim link initialized");
 
+  //////////////////////////////
+  // dynamic editing
+  //////////////////////////////
   function loadScore(){
-    //Converter Class
     var Converter=require("csvtojson").core.Converter;
     var fs=require("fs");
 
     var csvFileName="./public/score.csv";
     var fileStream=fs.createReadStream(csvFileName);
-    //new converter instance
     var csvConverter=new Converter({constructResult:true});
 
-    //end_parsed will be emitted once parsing finished
     csvConverter.on("end_parsed",function(jsonObj){
-       //console.log(jsonObj); //here is your result json object
-       io.sockets.emit("score", jsonObj);
+      io.sockets.emit("score", jsonObj);
     });
-
     //read from file
     fileStream.pipe(csvConverter);
   }
 
-  var util  = require('util'),
-      spawn = require('child_process').spawn;
+  //////////////////////////////
+  //  movies Management
+  //////////////////////////////
+  function refreshTimelaps( speed, zoom){
 
+    speed = typeof speed !== 'undefined' ? speed : 1;
+    zoom  = typeof zoom  !== 'undefined' ? zoom  : 1;
 
-  var oscServer = new osc.Server(3333, '0.0.0.0');
-  
-  oscServer.on("message", function (msg, rinfo) {
-    console.log(msg);
-    io.sockets.emit("oscMessage", msg);
+    var mov_w  = 1888,
+        mov_h  = 1062, 
+        ratio  = mov_w/mov_h,
+        crop_w = Math.round(mov_w*zoom),
+        crop_h = Math.round(crop_w/ratio),
+        crop_x = Math.round((crop_w - mov_w)/2),
+        crop_y = Math.round((crop_h - mov_h)/2),
+        proc = new ffmpeg({ source: 'public/exposure/im%02d.jpg' })
 
+        .withFps(25)
+        .addOptions(['-c:v libx264', '-preset ultrafast', '-qp 0'])
+        .addOptions(['-r 25', '-preset ultrafast', '-qp 0'])
+        .withVideoFilter('scale='+crop_w+':-1')
+        .withVideoFilter('crop='+mov_w+':'+mov_h+':'+crop_x+':'+crop_y+'')
+        .withVideoFilter('setpts=('+speed+'/1)*PTS')
+        .on('end', onRefreshTimelapsEnd)
+        .on('error', function(err) { console.log('an error happened: ' + err.message);})
+        .saveToFile('public/video/live.mp4');
+  }
+  function onRefreshTimelapsEnd(){
+    console.log('timelapse updated');
+  }
+  function initCapture(){
 
     /// SPAWN TEST 
-
+    spawn = require('child_process').spawn;
     capt = spawn('bash',['bin/test.sh']); 
     capt.stdout.on('data', function (data) {    // register one or more handlers
       console.log('stdout: ' + data);
@@ -60,20 +77,26 @@ module.exports = function(app, io, oscServer){
     }, 10000);
 
     /// END SPAWN TEST
+  }
 
+  //////////////////////////////
+  //  communication 
+  //////////////////////////////
+  var oscServer = new osc.Server(3333, '0.0.0.0');  
+  oscServer.on("message", function (msg, rinfo) {
+    console.log(msg);
+    io.sockets.emit("oscMessage", msg);
+
+    switch (msg[0]) {
+      case "/refreshTimelaps":
+        refreshTimelaps();
+      break;
+    };
   });
 
-  /**
-  * Socket.IO events
-  */
-
   io.on("connection", function(socket){
-    socket.on("message", function(obj) {
-      console.log(obj);
-      oscClient.send(obj);
-    });
-    socket.on("getScore", function(obj){
-      loadScore();
-    });
+    socket.on("message", oscClient.send);
+    socket.on("getScore", loadScore);
+    socket.on("refreshTimelaps", refreshTimelaps);
   });
 };
